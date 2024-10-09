0ea233ca5195f80d116917b9b982fd73ad7723e6
AuroreTnr <tournieraurore@orange.fr>
2024-10-09 14:36:01 +0200

    pas au point, probleme avec pourcentage à plusieurs chiffre

--- assets/script.js
+++ assets/script.js
@@ -13,7 +13,7 @@ for(const button of buttons){
 
 
 egal.addEventListener("click", resultat);
-clear.addEventListener("click", reset)
+clear.addEventListener("click", reset);
 
 
 
@@ -28,20 +28,113 @@ let expression;
 
 
 
+// CALCUL
 function calculatrice(e){
+    // CALCUL (+ - * /)
     if (interruptor) {
         value = e.target.getAttribute("id");
         arr.push(value)
 
-        input.value =  expression = arr.join(" ").replace("×", "*").replace("÷", "/").replace("=","").replaceAll(" ", "");
-    }
+        input.value =  expression = arr.join(" ").replace("=","").replaceAll(" ", "").replace("modulo", "%");
+        
+        // CALCUL %
+        if (expression.includes("%")) {
+            pourcentage()
     
+        }
+    }
+
+        
 }
 
 
+// CALCUL %
+function pourcentage(){
+
+    if(arr.includes("+")) {
+        console.log(arr);
+        
+        
+        let sliceNb1 = arr.slice(arr[1], (arr.indexOf("+"))).join("");
+        let sliceNb2 = arr.slice((arr.indexOf("+") + 1), arr[arr.length]).join("").replace("modulo", "").replace("=", "");
+
+
+        console.log(sliceNb1);
+        console.log(sliceNb2);
+        
+
+        expression = sliceNb2 + (sliceNb2 * sliceNb1/100);
+        
+        
+
+        // PAS BON
+
+    }
+    else if(arr.includes("-")) {
+        console.log(arr);
+        
+        let sliceNb1 = arr.slice(arr[1], (arr.indexOf("-"))).join("");
+        let sliceNb2 = arr.slice((arr.indexOf("-") + 1), arr[arr.length]).join("").replace("modulo", "").replace("=", "");
+
+        console.log(sliceNb1);
+        console.log(sliceNb2);
+
+        expression = sliceNb1 - (sliceNb1 * sliceNb2/100);
+
+        // OK
+
+    }
+    else if(arr.includes("*")) {
+        console.log(arr);
+        
+        let sliceNb1 = arr.slice(arr[1], (arr.indexOf("*"))).join("");
+        let sliceNb2 = arr.slice((arr.indexOf("*") + 1), arr[arr.length]).join("").replace("modulo", "").replace("=", "");
+
+        console.log(sliceNb1);
+        console.log(sliceNb2);
+
+        expression =  sliceNb1 * (sliceNb1 * sliceNb2/100);
+
+        // ????????????
+
+        // exemple 20 * 2%
+        // 2 % de 20 = 0.4
+        // 20 * 0.4 = 8
+
+
+    }
+    else if(arr.includes("/")) {
+        console.log(arr);
+        
+        let sliceNb1 = arr.slice(arr[1], (arr.indexOf("/"))).join("");
+        let sliceNb2 = arr.slice((arr.indexOf("/") + 1), arr[arr.length]).join("").replace("modulo", "").replace("=", "");
+
+        console.log(sliceNb1);
+        console.log(sliceNb2);
+
+        expression =  sliceNb1 / (sliceNb1 * sliceNb2/100);
+
+        // ????????????
+
+        // exemple 20 / 2%
+        // 2 % de 20 = 0.4
+        // 20 / 0.4 = 50
+
+
+    }
+
+}
+    
+
+
+
+// DISPLAY RESULTAT
 function resultat(){
     interruptor = false;
 
+    console.log(arr.join("").replace("modulo", "").replace("=", ""));
+    
+    
     try {
         input.value = eval(expression)
     }catch (error) {
@@ -50,6 +143,10 @@ function resultat(){
     
 }
 
+
+
+
+// RESET
 function reset(){
     interruptor = true;
     
